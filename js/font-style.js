75901109f663f8feff8901c5c52682e7d84a8a8e
Rezwan66 <shaikhrezwan66@gmail.com>
2023-08-17 16:01:45 +0600

    added underlined style, and found a consistent better way to change font style

--- js/font-style.js
+++ js/font-style.js
@@ -1,15 +1,49 @@
-document.getElementById('icon-bold').addEventListener('click',function(){
+// MAKE BOLD
+let isBold = false;
+document.getElementById('icon-bold').addEventListener('click', function () {
     const textArea = getTextAreaValue();
-    
-    textArea.classList.toggle('font-bold');
-    
-    setTextAreaValue(textArea);
+
+    if (!isBold) {
+        textArea.classList.add('font-bold');
+        isBold = true;
+    } else {
+        textArea.classList.remove('font-bold');
+        isBold = false;
+    }
+
+    // setTextAreaValue(textArea);
 })
 
-document.getElementById('icon-italic').addEventListener('click',function(){
+// MAKE ITALIC
+let isItalic = false;
+document.getElementById('icon-italic').addEventListener('click', function () {
     const textArea = getTextAreaValue();
-    
-    textArea.classList.toggle('italic');
-    
-    setTextAreaValue(textArea);
+
+    if (!isItalic) {
+        textArea.classList.add('italic');
+        isItalic = true;
+    } else {
+        textArea.classList.remove('italic');
+        isItalic = false;
+    }
+
+    // setTextAreaValue(textArea);
 })
+
+// MAKE UNDERLINE
+let isUnderlined = false;
+document.getElementById('icon-underlined').addEventListener('click',function(){
+    const textArea = getTextAreaValue();
+
+    if(!isUnderlined){
+        textArea.classList.add('underline');
+        isUnderlined = true;
+    }else{
+        textArea.classList.remove('underline');
+        isUnderlined = false;
+    }
+
+    // setTextAreaValue(textArea);
+})
+
+/* nb: We are not needing to use the setTextAreaValue(textArea) function at all since we are directly changing the text area's attribute or class name here, so nothing to do with/ no need to set the new .value of the text area.!!! */
